5582f019fafc86587a94c853ba8adb4338678813
slark-prime <43880036+slark-prime@users.noreply.github.com>
2023-07-03 22:16:37 +0800

    Add files via upload

--- CAQA.py
+++ CAQA.py
@@ -6,44 +6,15 @@ from langchain.callbacks import get_openai_callback
 from langchain.vectorstores import Chroma
 from constants import CHROMA_SETTINGS, PERSIST_DIRECTORY
 
-
 class CAQA:
-    def __init__(self, directoryPath, llmRepoId="openai", embeddingModel="hkunlp/instructor-xl"):
-        """
+    def __init__(self):
+        self.qa_chain = None
+        self.llm_repo_id = "openai"
+        self.embedding_model = "hkunlp/instructor-xl"
+        self.llm = None
+        self.llm_kwargs = {"temperature": 0.1, "max_new_tokens": 500}
 
-        :param directoryPath: context directory
-        :param llmRepoId: llm repo id in huggingface hub
-        :param embeddingModel: embedding model used
-        """
-        self.directoryPath = directoryPath
-        self.llmRepoId = llmRepoId
-        self.embeddingModel = embeddingModel
-
-        # embedding Model
-        if embeddingModel == "hkunlp/instructor-xl":
-            embeddings = HuggingFaceInstructEmbeddings(model_name=embeddingModel)
-
-        elif embeddingModel == "openai":
-            embeddings = OpenAIEmbeddings()
-
-        else:
-            embeddings = HuggingFaceEmbeddings(model_name=embeddingModel)
-
-        # load the vectorstore
-        db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-        retriever = db.as_retriever()
-
-
-
-        # LLM
-        if llmRepoId != "openai":
-            self.llm = HuggingFaceHub(repo_id=llmRepoId)
-        else:
-            self.llm = OpenAI()
-
-        self.qaChain = RetrievalQA.from_chain_type(llm=self.llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-
-    def generateResponse(self, query):
+    def generate_response(self, query):
         """
         :param query:
         :param chainType: type for load_qa_chain, can be: "stuff", "map_reduce", "map_rerank", "refine"
@@ -51,11 +22,56 @@ class CAQA:
         """
         with get_openai_callback() as cb:  # calculate token usage if openai LLM is used
             # build the qa chain
-            response = self.qaChain(query)
-            answer, sourceDocs = response['result'], response['source_documents']
+            response = self.qa_chain(query)
+            answer, source_docs = response['result'], response['source_documents']
 
-
-            if self.llmRepoId == "openai":
+            if self.llm_repo_id == "openai":
                 print(cb)
 
-        return answer, sourceDocs
+        return answer, source_docs
+
+
+class CAQABuilder:
+    def __init__(self):
+        self.caqa = CAQA()
+
+    def set_embedding_model(self, embedding_model):
+        self.caqa.embedding_model = embedding_model
+        return self
+
+    def set_llm(self, llm_repo_id):
+        self.caqa.llm_repo_id = llm_repo_id
+        return self
+
+    def set_llm_params(self, **kwargs):
+        self.caqa.llm_kwargs.update(kwargs)
+        return self
+
+    def build_qa_chain(self):
+        # embedding Model
+        if self.caqa.embedding_model == "hkunlp/instructor-xl":
+            embeddings = HuggingFaceInstructEmbeddings(model_name=self.caqa.embedding_model)
+
+        elif self.caqa.embedding_model == "openai":
+            embeddings = OpenAIEmbeddings()
+
+        else:
+            embeddings = HuggingFaceEmbeddings(model_name=self.caqa.embedding_model)
+
+        # load the vectorstore
+        db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+        retriever = db.as_retriever()
+
+        # LLM
+        if self.caqa.llm_repo_id != "openai":
+            self.llm = HuggingFaceHub(repo_id=self.caqa.llm_repo_id,
+                                      model_kwargs=self.caqa.llm_kwargs)
+        else:
+            self.caqa.llm = OpenAI()
+
+        self.caqa.qa_chain = RetrievalQA.from_chain_type(llm=self.llm, chain_type="stuff", retriever=retriever,
+                                                         return_source_documents=True)
+
+    def build(self):
+        self.build_qa_chain()
+        return self.caqa
--- ingest.py
+++ ingest.py
@@ -1,45 +1,30 @@
-import os
-from typing import List
-
-from langchain.document_loaders import TextLoader, PDFMinerLoader, CSVLoader
+from Loader.LocalDirectoryLoader import LocalDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.docstore.document import Document
 from constants import CHROMA_SETTINGS, SOURCE_DIRECTORY, PERSIST_DIRECTORY, INGEST_MODEL
 from langchain.embeddings import HuggingFaceInstructEmbeddings
-
 import torch
 
 torch.backends.mps.enabled = True
 torch.backends.mps.max_concurrency = 1
 device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 
-
-def load_single_document(file_path: str) -> Document:
-    # Loads a single document from a file path
-    if file_path.endswith(".txt"):
-        loader = TextLoader(file_path, encoding="utf8")
-    elif file_path.endswith(".pdf"):
-        loader = PDFMinerLoader(file_path)
-    elif file_path.endswith(".csv"):
-        loader = CSVLoader(file_path)
-    return loader.load()[0]
-
-
-def load_documents(source_dir: str) -> List[Document]:
-    # Loads all documents from source documents directory
-    all_files = os.listdir(source_dir)
-    return [load_single_document(f"{source_dir}/{file_path}") for file_path in all_files if
-            file_path[-4:] in ['.txt', '.pdf', '.csv']]
-
+EVAL = False
 
 def main():
     #  Load documents and split in chunks
-    print(f"Loading documents from {SOURCE_DIRECTORY}")
-    documents = load_documents(SOURCE_DIRECTORY)
+    dir_loader = LocalDirectoryLoader(SOURCE_DIRECTORY)
+    documents = dir_loader.load()
+
+    if EVAL:
+        from Eval.extract_blocks import extract_blocks_from_documents
+        extract_blocks_from_documents(documents, 30)
+
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
     texts = text_splitter.split_documents(documents)
-    print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+
+
     print(f"Split into {len(texts)} chunks of text")
 
     # Create embeddings
